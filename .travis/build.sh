66769e2b40e7fa578e712e1b6958e75664337f64
Michael Clausen <encodeering@gmail.com>
2016-01-09 15:20:52 +0100

    creates a debian based buildpack image
    * arch: armhf, amd64
    * version: jessie
    
    NOTE
    
    * required by other images, e.g. ruby

---
+++ .travis/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ev
+
+TAG="$REPOSITORY/$PROJECT-$ARCH"
+TAGSPECIFIER="$VERSION"
+BUILDTAGSPECIFIER="buildpack-deps:$VERSION"
+
+docker pull   "$REPOSITORY/debian-$ARCH:$VERSION"
+docker tag -f "$REPOSITORY/debian-$ARCH:$VERSION" "debian:$VERSION"
+
+docker build -t "$BUILDTAGSPECIFIER-curl" "$PROJECT/$VERSION/curl"
+docker build -t "$BUILDTAGSPECIFIER-scm"  "$PROJECT/$VERSION/scm"
+docker build -t "$BUILDTAGSPECIFIER"      "$PROJECT/$VERSION"
+
+docker tag   -f "$BUILDTAGSPECIFIER-curl" "$TAG:$TAGSPECIFIER-curl"
+docker tag   -f "$BUILDTAGSPECIFIER-scm"  "$TAG:$TAGSPECIFIER-scm"
+docker tag   -f "$BUILDTAGSPECIFIER"      "$TAG:$TAGSPECIFIER"
