1f4f4a61b5e14a7ba1d517e73a95bb5596745867
Michael Clausen <encodeering@gmail.com>
2020-05-15 20:18:50 +0200

    updating scm repository to latest revision
    * 4d7c3e93fd1cafae3e319925fb6dcf43537e1ca9

--- modules/build.sh
+++ modules/build.sh
@@ -6,8 +6,8 @@ import com.encodeering.ci.docker
 
 docker-pull "${REPOSITORY}/debian-${ARCH}:${VERSION}" "debian:${VERSION}"
 
-docker-build --suffix curl -t "buildpack-deps:${VERSION}-curl" "${PROJECT}/${VERSION}/curl"
-docker-build --suffix scm  -t "buildpack-deps:${VERSION}-scm"  "${PROJECT}/${VERSION}/scm"
-docker-build               -t "buildpack-deps:${VERSION}"      "${PROJECT}/${VERSION}"
+docker-build --suffix curl -t "buildpack-deps:${VERSION}-curl" "${PROJECT}/debian/${VERSION}/curl"
+docker-build --suffix scm  -t "buildpack-deps:${VERSION}-scm"  "${PROJECT}/debian/${VERSION}/scm"
+docker-build               -t "buildpack-deps:${VERSION}"      "${PROJECT}/debian/${VERSION}"
 
 docker-verify cat /etc/os-release | dup | contains "(${VERSION})"
